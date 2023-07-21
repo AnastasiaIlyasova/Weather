57902902887c64f6880c12d370670d1572262654
AnastasiaIlyasova <destinyps@mail.ru>
2023-07-21 06:14:48 +0300

    commit 1

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 
-//Request location
-/*navigator.geolocation.getCurrentPosition(function(position) {
-    //findNear();
-});
-
-
-if ("geolocation" in navigator) {
-alert('kr')
-} else {
-alert('nio')
-}*/
-
-
-
+const apiKey = '825dcc91205501c25f7a9e1bfb75bdc2'
+let city = ''
 const success = (position) =>{
 
     var url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/geolocate/address";
@@ -30,185 +18,80 @@ const success = (position) =>{
         },
         body: JSON.stringify(query)
     }
+    function changeBg(data) {
+        if (data.weather[0].main === 'Clouds') {
+            document.body.removeAttribute('class');
+            document.body.classList.add('bg-cloudy');
+        }
+        if (data.weather[0].main === 'Clear') {
+            document.body.removeAttribute('class');
+            document.body.classList.add('bg-clear');
+        }
+        if (data.weather[0].main === 'Rain') {
+            document.body.removeAttribute('class');
+            document.body.classList.add('bg-rain');
+        }
+    }
+
+    function addInfo(data){
+        document.querySelector('.user__city').textContent = ''
+        //добавляем название города
+        document.querySelector('.weather__city').textContent = data.name;
+        //data.main.temp содержит значение в Кельвинах, отнимаем от  273, чтобы получить значение в градусах Цельсия
+        document.querySelector('.weather__forecast').innerHTML = Math.round(data.main.temp - 273) + '&deg;' + 'C';
+        //Добавляем описание погоды
+        document.querySelector('.weather__desc').textContent = data.weather[0]['description'];
+        //Добавляем иконку погоды
+        document.querySelector('.weather__icon').innerHTML = `<img width="35" height="30" src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
+        document.querySelector('.weather__wind').textContent = ` Wind speed: ${data.wind.speed} m/s`;
+        document.querySelector('.weather__humidity').textContent = `Humidity: ${data.main.humidity}%`
+    }
 
     fetch(url, options)
         .then(response => response.json())
         .then(function (data) {
-            document.querySelector('.user__city').textContent = ` Your current city: ${data.suggestions[0].value.split(',')[1].split(' ')[2]}`;
- currentCity = data.suggestions[0].value.split(',')[1].split(' ')[2]
+            document.querySelector('.user__city').textContent = ` Your current city: ${data.suggestions[0].data.city}`;
+ currentCity = data.suggestions[0].data.city
             console.log(data)})
-        .then(function (){
-            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${apiKey}`).then(function (resp) {return resp.json() }).then(function (data) {
+        .then(function () {
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${apiKey}`).then(function (resp) {
+                return resp.json()
+            }).then(function (data) {
                 console.log(currentCity)
+                addInfo(data)
+                document.querySelector('.user__city').textContent = `Your current city: ${currentCity}`
+                changeBg(data)
 
+                function enter() {
+                            document.querySelector('input').addEventListener('keydown', function (e) {
+                                if (e.keyCode === 13) {
+                                    getInput()
+                                }
+                            });
+                        }
+                        enter()
 
+                        let btnValue = document.getElementById("btn");
+                        btnValue.addEventListener('click', getInput);
 
-                //добавляем название города
-                document.querySelector('.weather__city').textContent = data.name;
-                //data.main.temp содержит значение в Кельвинах, отнимаем от  273, чтобы получить значение в градусах Цельсия
-                document.querySelector('.weather__forecast').innerHTML = Math.round(data.main.temp - 273) + '&deg;'+'C';
-                //Добавляем описание погоды
-                document.querySelector('.weather__desc').textContent = data.weather[0]['description'];
-                //Добавляем иконку погоды
-                document.querySelector('.weather__icon').innerHTML = `<img width="35" height="30" src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
-                document.querySelector('.weather__wind').textContent = ` Wind speed: ${data.wind.speed} m/s`;
-                document.querySelector('.weather__humidity').textContent = `Humidity: ${data.main.humidity}%`;
-                console.log(data.weather[0].main)
-                if(data.weather[0].main === 'Clouds'){
-                    document.body.removeAttribute('class');
-                    document.body.classList.add('bg-cloudy');
-                }if(data.weather[0].main === 'Clear'){
-                    document.body.removeAttribute('class');
-                    document.body.classList.add('bg-clear');
-                }if(data.weather[0].main === 'Rain'){
-                    document.body.removeAttribute('class');
-                    document.body.classList.add('bg-rain');
-                };
-
-            })
-              /*  .then(function (){
-                    if(data.weather[0].main === 'Clouds'){
-                        document.body.removeAttribute('class');
-                        document.body.classList.add('bg-cloudy');
-                    }if(data.weather[0].main === 'Clear'){
-                        document.body.removeAttribute('class');
-                        document.body.classList.add('bg-clear');
-                    }if(data.weather[0].main === 'Rain'){
-                        document.body.removeAttribute('class');
-                        document.body.classList.add('bg-rain');
-                    };
-               // })*/
-
-                .catch(function () {
-                    //Обрабатываем ошибки
-                })
+                        function getInput() {
+                            city = document.getElementById('text').value.trim();
+                            console.log(city);
+                            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`).then(function (resp) {
+                                return resp.json()
+                            }).then(function (data) {
+                                console.log(data)
+                                addInfo(data)
+                                changeBg(data)
+                            })
+                        }
+                    })
+                    .catch(error => console.log("error", error));
         })
-        .catch(error => console.log("error", error));
-        }
-
-
-
-
-
-
-
-
-
-
-
-    const apiKey = '825dcc91205501c25f7a9e1bfb75bdc2'
-    let city = ''
-
-    function enter() {
-        document.querySelector('input').addEventListener('keydown', function(e) {
-            if (e.keyCode === 13) {
-getInput()
-            }
-        });
-    }
-enter()
-    let btnValue = document.getElementById("btn");
-    btnValue.addEventListener('click', getInput);
-    function getInput() {
-        city = document.getElementById('text').value.trim();
-        console.log(city);
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`).then(function (resp) {return resp.json() }).then(function (data) {
-console.log(data)
-            document.querySelector('.user__city').textContent = ''
-            //добавляем название города
-            document.querySelector('.weather__city').textContent = data.name;
-            //data.main.temp содержит значение в Кельвинах, отнимаем от  273, чтобы получить значение в градусах Цельсия
-            document.querySelector('.weather__forecast').innerHTML = Math.round(data.main.temp - 273) + '&deg;'+'C';
-            //Добавляем описание погоды
-            document.querySelector('.weather__desc').textContent = data.weather[0]['description'];
-            //Добавляем иконку погоды
-            document.querySelector('.weather__icon').innerHTML = `<img width="35" height="30" src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
-            document.querySelector('.weather__wind').textContent = ` Wind speed: ${data.wind.speed} m/s`;
-            document.querySelector('.weather__humidity').textContent = `Humidity: ${data.main.humidity}%`;
-            if(data.weather[0].main === 'Clouds'){
-                document.body.removeAttribute('class');
-                document.body.classList.add('bg-cloudy');
-            }if(data.weather[0].main === 'Clear'){
-                document.body.removeAttribute('class');
-                document.body.classList.add('bg-clear');
-            }if(data.weather[0].main === 'Rain'){
-                document.body.removeAttribute('class');
-                document.body.classList.add('bg-rain');
-            }if(data.weather[0].main === 'Clouds'){
-                document.body.removeAttribute('class');
-                document.body.classList.add('bg-cloudy');
-            }
-        })
-           /* .then(function (){
-                if(data.weather[0].main === 'Clouds'){
-                    document.body.removeAttribute('class');
-                    document.body.classList.add('bg-cloudy');
-                }if(data.weather[0].main === 'Clear'){
-                    document.body.removeAttribute('class');
-                    document.body.classList.add('bg-clear');
-                }if(data.weather[0].main === 'Rain'){
-                    document.body.removeAttribute('class');
-                    document.body.classList.add('bg-rain');
-                }if(data.weather[0].main === 'Clouds'){
-                    document.body.removeAttribute('class');
-                    document.body.classList.add('bg-cloudy');
-                }
-          //  })*/
-            .catch(function () {
-                //Обрабатываем ошибки
-            })
-
-    }
-
-
+  }
 const error = () =>{
     const refuse = document.querySelector('.text__no__permission')
     refuse.style.display = 'block'
 }
 
 navigator.geolocation.getCurrentPosition(success, error)
-
-
-/*const apiKey = '825dcc91205501c25f7a9e1bfb75bdc2'
-let city = ''
-let btnValue = document.getElementById("btn");
-btnValue.addEventListener('click', getInput);
-function getInput() {
-     city = document.getElementById('text').value.trim();
-    console.log(city);
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`).then(function (resp) {return resp.json() }).then(function (data) {
-
-        //добавляем название города
-        document.querySelector('.weather__city').textContent = data.name;
-        //data.main.temp содержит значение в Кельвинах, отнимаем от  273, чтобы получить значение в градусах Цельсия
-        document.querySelector('.weather__forecast').innerHTML = Math.round(data.main.temp - 273) + '&deg;';
-        //Добавляем описание погоды
-        document.querySelector('.weather__desc').textContent = data.weather[0]['description'];
-        //Добавляем иконку погоды
-        document.querySelector('.weather__icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
-    })
-
-        .catch(function () {
-            //Обрабатываем ошибки
-        })
-
-}*/
-
-
-
-
-
-
-/*
-let arrWeather = []
-async function getResponse(){
-    let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
-    arrWeather = await response.json()
-    return arrWeather
-
-}
-getResponse()
-
-
-
-*/
